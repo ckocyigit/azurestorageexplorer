f863e24c447e6f7bc50eebf835c891e97e600ba7
Can Kocyigit <60385015+ckocyigit@users.noreply.github.com>
2023-09-14 14:44:20 +0200

    swap orders

--- src/web/Program.cs
+++ src/web/Program.cs
@@ -11,17 +11,17 @@ builder.Services.AddServerSideBlazor();
 
 var app = builder.Build();
 
+if (!string.IsNullOrEmpty(basePath))
+{
+    app.UsePathBase(basePath);
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
-
-if (!string.IsNullOrEmpty(basePath))
-{
-    app.UsePathBase(basePath);
 }
 
 app.UseHttpsRedirection();
